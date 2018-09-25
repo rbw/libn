a7ca8f961fe1c63cbd8535536ebeed285db4d119
Robert Wikman <rbw@vault13.org>
2018-09-25 20:38:46 +0200

    upstream fixes, adds omp (MacOS)

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ else:
 
 setup(
     name="libn",
-    version='0.1.1',
+    version='0.1.2',
     packages=['libn'],
     description='Python implementation of NANO-related functions.',
     url='https://github.com/rbw/libn',
